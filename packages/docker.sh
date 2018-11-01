6ea43ec90698589848a0f23fbddf0fe27ba38e5f
Greg Lawrance <greg.lawrance@gov.bc.ca>
2018-10-31 20:25:53 -0700

    update release  of docker compose

--- packages/docker.sh
+++ packages/docker.sh
@@ -24,7 +24,7 @@ sudo usermod -aG docker $USER
 
 # docker-compose
 
-sudo curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+sudo curl -L https://github.com/docker/compose/releases/download/1.22.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
 
 sudo chmod +x /usr/local/bin/docker-compose
 
